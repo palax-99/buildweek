9763b8eda54b8298fd08a0bb68d13608c9acf5bc
Raviolz <gi.ravioli@gmail.com>
2026-04-27 15:22:12 +0200

    PrivateTest

---
+++ src/main/java/finalBW/buildweek/repository/RuoloRepository.java
@@ -0,0 +1,7 @@
+package finalBW.buildweek.repository;
+
+import finalBW.buildweek.entity.Ruolo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RuoloRepository extends JpaRepository<Ruolo, Long> {
+}
