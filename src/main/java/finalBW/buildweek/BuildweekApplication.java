20702e977220bd7ca0e60e551ff319df223b1984
palax-99 <ninociao7@gmail.com>
2026-04-27 11:40:14 +0200

    Initial commit

---
+++ src/main/java/finalBW/buildweek/BuildweekApplication.java
@@ -0,0 +1,13 @@
+package finalBW.buildweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildweekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finalBW/buildweek/BuildweekApplicationTests.java
@@ -0,0 +1,13 @@
+package finalBW.buildweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildweekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
