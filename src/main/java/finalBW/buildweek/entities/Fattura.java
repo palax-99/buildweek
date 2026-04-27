029552611c7f1155fcf77f2200706b84dd7f1f25
palax-99 <ninociao7@gmail.com>
2026-04-27 14:09:49 +0200

    Create the entity: clienti, fattura, stato fattura

---
+++ src/main/java/finalBW/buildweek/entities/Fattura.java
@@ -0,0 +1,49 @@
+package finalBW.buildweek.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "fatture")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Fattura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long fatturaId;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private BigDecimal importo;
+
+    @Column(nullable = false, unique = true)
+    private Long numero;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stato_fattura_id", nullable = false)
+    private StatoFattura statoFattura;
+
+    public Fattura(LocalDate data, BigDecimal importo, Long numero,
+                   Cliente cliente, StatoFattura statoFattura) {
+        this.data = data;
+        this.importo = importo;
+        this.numero = numero;
+        this.cliente = cliente;
+        this.statoFattura = statoFattura;
+    }
+}
