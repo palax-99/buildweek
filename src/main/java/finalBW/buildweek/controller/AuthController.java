2f0d4dc809158fb245666f429e13d30ac24338b0
Samuel-Valentini <samuelvitalia@gmail.com>
2026-04-29 11:19:00 +0200

    register endpoint implemented

--- src/main/java/finalBW/buildweek/controller/AuthController.java
+++ src/main/java/finalBW/buildweek/controller/AuthController.java
@@ -1,23 +1,48 @@
 package finalBW.buildweek.controller;
 
+import finalBW.buildweek.entity.Utente;
+import finalBW.buildweek.exceptions.BadRequestException;
 import finalBW.buildweek.payload.LoginDTO;
 import finalBW.buildweek.payload.LoginResponseDTO;
+import finalBW.buildweek.payload.NewUtenteResponseDTO;
+import finalBW.buildweek.payload.NuovoUtenteDTO;
 import finalBW.buildweek.service.AuthService;
+import finalBW.buildweek.service.UtenteService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final UtenteService utenteService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, UtenteService utenteService) {
         this.authService = authService;
+        this.utenteService = utenteService;
     }
 
     @PostMapping("/login")
     public LoginResponseDTO login(@RequestBody @Valid LoginDTO body) {
         return authService.login(body);
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public NewUtenteResponseDTO saveUtente(@RequestBody @Validated NuovoUtenteDTO nuovoUtenteDTO, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            List<String> errors = validationResult.getFieldErrors().stream().map(error -> error.getDefaultMessage()).toList();
+            throw new BadRequestException(errors);
+        }
+
+        Utente newUtente = utenteService.save(nuovoUtenteDTO);
+        return new NewUtenteResponseDTO(newUtente.getId());
+
     }
 }
--- src/main/java/finalBW/buildweek/exceptions/BadRequestException.java
+++ src/main/java/finalBW/buildweek/exceptions/BadRequestException.java
@@ -1,7 +1,13 @@
 package finalBW.buildweek.exceptions;
 
+import java.util.List;
+
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
+    }
+
+    public BadRequestException(List<String> messageList) {
+        super(String.join(". ", messageList));
     }
 }
